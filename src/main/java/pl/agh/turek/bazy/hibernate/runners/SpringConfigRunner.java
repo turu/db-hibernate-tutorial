c5c78741d76e869625c4eda1e63cd35d4cfff001
turek <turek@a9e82a19-4f19-4be0-97f0-353e01fcac2c>
2014-04-02 14:53:52 +0000

    spring config runner init
    git-svn-id: https://lab.iisg.agh.edu.pl/svn/rm_dydakt/BD2_14L_G/trunk@4636 a9e82a19-4f19-4be0-97f0-353e01fcac2c

--- src/main/java/pl/agh/turek/bazy/hibernate/runners/SpringConfigRunner.java
+++ src/main/java/pl/agh/turek/bazy/hibernate/runners/SpringConfigRunner.java
@@ -1,11 +1,18 @@
 package pl.agh.turek.bazy.hibernate.runners;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Author: Piotr Turek
  */
 public class SpringConfigRunner {
 
     public static void main(String[] args) {
+        new SpringConfigRunner().run();
+    }
+
+    private void run() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
     }
 
 }
