48188cc152a89a3cadc70860cd86f6ac5d188d75
turu <piotr.turu.turek@gmail.com>
2014-04-27 22:20:35 +0200

    fix territories entity

--- src/main/java/pl/agh/turek/bazy/hibernate/runners/ObjectStatesCaseRunner.java
+++ src/main/java/pl/agh/turek/bazy/hibernate/runners/ObjectStatesCaseRunner.java
@@ -3,15 +3,11 @@ package pl.agh.turek.bazy.hibernate.runners;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.agh.turek.bazy.hibernate.model.TerritoriesEntity;
 import pl.agh.turek.bazy.hibernate.repository.TerritoriesDao;
 
 public class ObjectStatesCaseRunner {
-    @Autowired
-    private TerritoriesDao dao;
-
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SpringConfigRunner.class);
 
     public static void main(String[] args) {
@@ -21,6 +17,8 @@ public class ObjectStatesCaseRunner {
     private void run() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
         SessionFactory sessionFactory = (SessionFactory) ctx.getBean("sessionFactory");
+        TerritoriesDao territoriesDao = (TerritoriesDao) ctx.getBean("territoriesDao");
+
         Session session = sessionFactory.openSession();
 
         /**
@@ -33,7 +31,7 @@ public class ObjectStatesCaseRunner {
         /**
          * we wont receive any result - transient object is not persisted
          */
-        TerritoriesEntity foundEntity = dao.get("Transient");
+        TerritoriesEntity foundEntity = territoriesDao.get("Transient");
         System.out.println(foundEntity);
 
 
