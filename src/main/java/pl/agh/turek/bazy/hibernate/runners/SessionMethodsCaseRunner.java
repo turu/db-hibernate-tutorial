570d70a0d4f862eb4304a696611d38e17ae3e4d2
zmuda <ferry.2>
2014-04-28 10:51:19 +0200

    [session methods] runner init

---
+++ src/main/java/pl/agh/turek/bazy/hibernate/runners/SessionMethodsCaseRunner.java
@@ -0,0 +1,49 @@
+package pl.agh.turek.bazy.hibernate.runners;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.agh.turek.bazy.hibernate.model.TerritoriesEntity;
+import pl.agh.turek.bazy.hibernate.repository.TerritoriesDao;
+
+public class SessionMethodsCaseRunner {
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(SpringConfigRunner.class);
+
+    public static void main(String[] args) {
+        new SessionMethodsCaseRunner().run();
+    }
+
+    private void run() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("META-INF/applicationContext.xml");
+        SessionFactory sessionFactory = (SessionFactory) ctx.getBean("sessionFactory");
+        TerritoriesDao territoriesDao = (TerritoriesDao) ctx.getBean("territoriesDao");
+
+        Session session = sessionFactory.openSession();
+
+        TerritoriesEntity exampleEntity = new TerritoriesEntity();
+        exampleEntity.setTerritoryId("Example");
+        exampleEntity.setTerritoryDescription("This is Example");
+
+        session.save(exampleEntity);
+        session.flush();
+        TerritoriesEntity foundEntity = territoriesDao.get("Example");
+        System.out.println(foundEntity.getTerritoryDescription());
+
+        exampleEntity.setTerritoryDescription("This is still Example");
+
+        session.update(exampleEntity);
+        session.flush();
+        foundEntity = territoriesDao.get("Example");
+        System.out.println(foundEntity.getTerritoryDescription());
+
+        session.delete(exampleEntity);
+        session.flush();
+        foundEntity = territoriesDao.get("Example");
+        System.out.println(foundEntity);
+
+        session.close();
+    }
+
+
+}
